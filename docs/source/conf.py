930355fd17c5ac714e4d3fcf9e208de7468d7eec
Manuel Klar <klar@uni-trier.de>
2019-08-12 11:04:18 +0200

    First running version.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
-    'sphinx.ext.imgmath',
+    'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages']
