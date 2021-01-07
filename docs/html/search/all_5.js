1cf3e5d266d6b1ca8f8dd9437234fef9569de9f1
Klar, Manuel <klar@uni-trier.de>
2021-01-07 15:06:53 +0100

    Readme...

--- docs/html/search/all_5.js
+++ docs/html/search/all_5.js
@@ -3,6 +3,7 @@ var searchData=
   ['kernel_5fconstant',['kernel_constant',['../model_8cpp.html#a066dc1138361f907a289bec6af1cf67b',1,'model.cpp']]],
   ['kernel_5fconstant1d',['kernel_constant1D',['../model_8cpp.html#a248a983cf1917d574ff79b859b228ab1',1,'model.cpp']]],
   ['kernel_5fconstant3d',['kernel_constant3D',['../model_8cpp.html#adc4dd5c804f46fe5711af180dfab02dd',1,'model.cpp']]],
+  ['kernel_5fconstanttruncated',['kernel_constantTruncated',['../model_8cpp.html#a92ff1f5e6d8d1a4edcbf29267046f81b',1,'model.cpp']]],
   ['kernel_5flabeled',['kernel_labeled',['../model_8cpp.html#adbaa45ce6ef8512216d6f683f4cb4836',1,'model.cpp']]],
   ['kernel_5flinearprototypemicroelastic',['kernel_linearPrototypeMicroelastic',['../model_8cpp.html#a3dbe3924698ddd392399ac83b81829c9',1,'model.cpp']]],
   ['kernel_5fparabola',['kernel_parabola',['../model_8cpp.html#a3b0ce634face2703b19e3cd9a876fb14',1,'model.cpp']]],
--- docs/html/search/functions_5.js
+++ docs/html/search/functions_5.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['par_5fassemble',['par_assemble',['../Cassemble_8cpp.html#ab83f9055a5357703354de301ba41ca7a',1,'Cassemble.cpp']]],
-  ['par_5fevaluatemass',['par_evaluateMass',['../Cassemble_8cpp.html#a3210fd58a3c11bda095949e1465c6b70',1,'Cassemble.cpp']]],
+  ['par_5fevaluatemass',['par_evaluateMass',['../Cassemble_8cpp.html#a70ea29197a77e81437a7fd4071c8d179',1,'Cassemble.cpp']]],
   ['par_5fforcing',['par_forcing',['../Cassemble_8cpp.html#af5d9ec675a5530d98e7e11f9686bee6c',1,'Cassemble.cpp']]],
-  ['par_5fsystem',['par_system',['../Cassemble_8cpp.html#a3bbd07ec6c5b6e2266b97b79e52240b0',1,'Cassemble.cpp']]]
+  ['par_5fsystem',['par_system',['../Cassemble_8cpp.html#ab127e495e002e3d96f4d2fb67888c637',1,'Cassemble.cpp']]]
 ];
